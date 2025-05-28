f9f1a856db9824622171690e1622663f7a333fc7
iyancoder <iyanuoluwaadebayo04@gmail.com>
2025-05-28 08:33:38 -0700

    EDA and Problems statement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements(file_path:str)->List[str]:
     '''
     requirements=[]
     with open (file_path) as file_obj:
-        requirements = file_obj.readlines()
+        requirements = file_obj.readline()
         requirements = [req.replace("\n"," ") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
