a5e41581e9a01cd094273576119ebc23443c0bac
iyancoder <iyanuoluwaadebayo04@gmail.com>
2025-05-29 06:50:11 -0700

    Hyperparameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -12,7 +12,7 @@ from sklearn.ensemble import (
 )
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.neighbors import KNeighborsClassifier  # ❌ Should be KNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor  # ❌ Should be KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
@@ -22,6 +22,7 @@ from src.logger import logging
 
 # Utilities to save objects and evaluate models
 from src.utils import save_object, evaluate_models
+
 
 # Configuration class to hold model path
 @dataclass
@@ -52,15 +53,58 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Regressor": KNeighborsClassifier(),  # ❌ Should use KNeighborsRegressor
+                "K-Neighbors Regressor": KNeighborsRegressor(),  # ❌ Should use KNeighborsRegressor
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=0),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
+            ## Dictionatry models with hyperparmeters for hypertuning
+            param={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+                "K-Neighbors Regressor": {
+
+                },
+
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
 
             # Evaluate models using the utility function
             model_report: dict = evaluate_models(
-                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models
+                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models,param=param
             )
 
             # Select the model with the best R² score
@@ -86,7 +130,10 @@ class ModelTrainer:
             predicted = best_model.predict(X_test)
             r2_square = r2_score(y_test, predicted)
 
+           
+
             return r2_square
+        
 
         except Exception as e:
             raise CustomException(sys, e)
--- src/utils.py
+++ src/utils.py
@@ -1,46 +1,63 @@
 import os
 import sys
-
 import numpy as np
 import pandas as pd
 import dill
-from src.exception import CustomException
 
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 
+from src.exception import CustomException
+
+# -----------------------------
+# Save any Python object using dill
+# -----------------------------
 def save_object(file_path, obj):
     try:
+        # Create directory if it doesn't exist
         dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
 
-        os.makedirs(dir_path, exist_ok = True)
-
+        # Save the object to the specified path
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
+        # Raise a custom exception with full traceback info
+        raise CustomException(e, sys)
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+# -----------------------------
+# Train, tune, and evaluate multiple ML models
+# -----------------------------
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            # Safely get hyperparameters for the current model
+            model_params = param.get(model_name, {})
 
-            model.fit(X_train,y_train)
+            # Perform hyperparameter tuning using GridSearchCV
+            gs = GridSearchCV(model, model_params, cv=3)
+            gs.fit(X_train, y_train)
 
+            # Set best hyperparameters to model and train again
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # Predictions for train and test
             y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
 
-            y_test_pred = model. predict(X_test)
+            # R² scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
-            train_model_score = r2_score(y_train,y_train_pred)
-
-            test_model_score = r2_score(y_test,y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            # Store only test R² score for reporting
+            report[model_name] = test_model_score
 
         return report
-    
+
     except Exception as e:
-        raise CustomException(sys,e)
+        raise CustomException(e, sys)
